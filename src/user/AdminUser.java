e58cd210a237879581065bbf6ac07ed343af4c7e
yxmmyo <yxmmyo@163.com>
2021-06-16 22:30:40 +0800

    图书管理系统管理员代码

---
+++ src/user/AdminUser.java
@@ -0,0 +1,36 @@
+package user;
+
+import com.sun.corba.se.spi.orb.Operation;
+import operation.*;
+
+import java.util.Scanner;
+
+public class AdminUser extends User{
+
+    public AdminUser(String name) {
+        super(name);
+        this.operations = new IOperation[] {
+                new ExitOperation(),
+                new FindOperation(),
+                new AddOperation(),
+                new DelOperation(),
+                new DisplayOperation()
+        };
+    }
+
+    @Override
+    public int menu () {
+        System.out.println("=============================");
+        System.out.println("Hello " + this.name + ", 欢迎使用图书管理系统!");
+        System.out.println("1. 查找图书");
+        System.out.println("2. 新增图书");
+        System.out.println("3. 删除图书");
+        System.out.println("4. 显示所有图书");
+        System.out.println("0. 退出系统");
+        System.out.println("=============================");
+        System.out.println("请输入您的选择: ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        return choice;
+    }
+}
