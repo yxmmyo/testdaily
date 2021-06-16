64cce8135100146d8f3981cc96959a36e7e36f61
yxmmyo <yxmmyo@163.com>
2021-06-16 22:31:16 +0800

    图书管理系统普通人代码

---
+++ src/user/NormalUser.java
@@ -0,0 +1,33 @@
+package user;
+
+import book.BookList;
+import operation.*;
+import java.util.Scanner;
+
+public class NormalUser extends User{
+
+    public NormalUser(String name) {
+        super(name);
+        this.operations = new IOperation[] {
+                new ExitOperation(),
+                new FindOperation(),
+                new BorrowOperation(),
+                new ReturnOperation(),
+        };
+    }
+
+    @Override
+    public int menu() {
+        System.out.println("=============================");
+        System.out.println("Hello " + this.name + ", 欢迎使用图书管理系统!");
+        System.out.println("1. 查找图书");
+        System.out.println("2. 借阅图书");
+        System.out.println("3. 归还图书");
+        System.out.println("0. 退出系统");
+        System.out.println("=============================");
+        System.out.println("请输入您的选择: ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        return choice;
+    }
+}
