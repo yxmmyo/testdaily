63ac0658b65062b5f0185b0a1f12eb62ac87b1fb
yxmmyo <yxmmyo@163.com>
2021-07-06 21:19:22 +0800

    构造二叉树

---
+++ TreeNode.java
@@ -0,0 +1,10 @@
+package impl;
+
+public  class TreeNode {
+    protected TreeNode left;
+    protected TreeNode right;
+    int val;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
