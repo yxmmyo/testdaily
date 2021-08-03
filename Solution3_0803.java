c010e8a4c2050ae0828868dc5934237479a1702e
yxmmyo <yxmmyo@163.com>
2021-08-03 22:43:40 +0800

    给定一个链表，判断链表中是否有环。

---
+++ Solution3_0803.java
@@ -0,0 +1,16 @@
+package listNode;
+
+public class Solution3_0803 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
