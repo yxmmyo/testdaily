564d98cf9daecb9bc51b3ce506488694442042ad
yxmmyo <yxmmyo@163.com>
2021-09-03 20:19:40 +0800

    IO

---
+++ ListFile.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListFile {
+
+    public static void main(String[] args) {
+
+        File dir = new File("E:\\javaCode\\code");
+        List<File> list = listDir(dir);
+        //jdk1.8集合框架使用stream操作，可以使用lambda表达式
+        list.stream().forEach(System.out::println);
+
+    }
+
+    public static List<File> listDir(File dir) {
+        List<File> list = new ArrayList<>();
+        if (dir.isFile()) {
+            list.add(dir);
+        } else if (dir.isDirectory()) {
+            File[] children = dir.listFiles();
+            if (children != null) {
+                for (File child : children) {
+                    List<File> files = listDir(child);
+                    list.addAll(files);
+                }
+            }
+        }
+        return list;
+    }
+}
